c6d964bf72a626d8ce87dbf7f1cd58c158bf5668
German <germanz20000@yandex.ru>
2020-09-30 14:53:02 +0400

    1

--- l4.R
+++ l4.R
@@ -1,5 +1,5 @@
 sozdanie <- function(){
-  setwd("C:/Users/GG/Desktop/4")
+  setwd("C:/Users/GG/Desktop")
   v <- read.table("fl.txt", header = FALSE)
   v1 = readline("Введите: ")
   df <- data.frame(v, v1)
